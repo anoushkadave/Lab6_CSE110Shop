2723718cf328c2482c5e28e377b4a33dea0629c5
Anoushka Dave <anoushkadave@MacBook-Air.lan>
2021-02-13 19:59:42 -0800

    Lab complete

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,103 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor(id, imageSrc, title, price, inCart) {
+    super();
+    this.id = id;
+    this.imageSrc = imageSrc;
+    this.title = title;
+    this.price = price;
+    this.inCart = inCart;
+
+    // shadow root
+    let shadow = this.attachShadow({mode: 'open'});
+
+    // apply external styles to shadow dom
+    // attach create element to the shadow dom
+    const linkElem = document.createElement('link');
+    linkElem.rel = 'stylesheet';
+    linkElem.href = '/styles/styles.css';
+    shadow.appendChild(linkElem);
+
+    // create new list element this product, set class name
+    const li = document.createElement('li');
+    li.className = "product";
+
+    // create image for this product, set src & alt
+    const img = document.createElement('img');
+    img.src = this.imageSrc;
+    img.alt = this.title;
+    li.appendChild(img);
+
+    // create title for this product
+    const pTitle = document.createElement('p');
+    pTitle.className = "title";
+    pTitle.textContent = this.title;
+    li.appendChild(pTitle);
+
+    // create price for this product
+    const pPrice = document.createElement('p');
+    pPrice.className = "price";
+    pTitle.textContent = this.price;
+    li.appendChild(pPrice);
+
+    // create button for this product
+    const button = document.createElement('button');
+
+    // update button text based on whether already in cart
+    if (this.inCart == false) {
+      button.textContent = "Add to Cart";
+    }
+    else {
+      button.textContent = "Remove from Cart";
+    }
+
+    // number of items in cart
+    let cartCount = document.getElementById("cart-count");
+
+    button.addEventListener("click", () => {
+      // cart from local storage
+      let cart = localStorage.getItem('cart');
+      let cartObj = JSON.parse(cart);
+
+      if (button.textContent == "Add to Cart") {
+        // change button title 
+        button.textContent = "Remove from Cart";
+
+        // increment cart count
+        let newCartCount = Number(cartCount.textContent) + 1;
+        cartCount.textContent = newCartCount;
+
+        // add item to the cart
+        cartObj.push(this.id);
+      }
+      else {
+        // change button title
+        button.textContent = "Add to Cart";
+
+        // decrement cart count
+        let newCartCount = Number(cartCount.textContent) - 1;
+        cartCount.textContent = newCartCount;
+
+        // remove item from the cart
+        for (let i = 0; i < cartObj.length; i++) {
+          if (Number(cartObj[i]) === Number(this.id)) {
+            cartObj.splice(i, 1);
+          }
+        }
+      }
+
+      // update cart in local storage
+      localStorage.setItem('cart', JSON.stringify(cartObj));
+    });
+
+    // add button to product item
+    li.appendChild(button);
+
+    // add newly created product item to product list
+    const ul = document.getElementById("product-list");
+    ul.appendChild(li);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,53 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  // if cart not aleady in local storage, create it
+  if (localStorage.getItem('cart') === null) {
+    let cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  // if items not already in local storage, add, create items
+  if (localStorage.getItem('items') === null) {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localStorage.setItem('items', JSON.stringify(data)))
+    .then(createItems());
+  }
+  // if items already in local storage, just create items
+  else {
+    createItems();
+  }
 });
+
+function createItems() {
+  // get the items
+  const items = localStorage.getItem('items');
+  const itemsObj = JSON.parse(items);
+
+  // get cart from local storage
+  const cart = localStorage.getItem('cart');
+  const cartObj = JSON.parse(cart);
+
+  // update cart size based on cart in local storage
+  const cartSize = document.getElementById("cart-count");
+  cartSize.textContent = Number(cartObj.length);
+
+  // product list
+  const ul = document.getElementById("product-list");
+
+  for (let i = 0; i < itemsObj.length; i++) {
+    // check whether item is already in cart from before
+    let inCart = false;
+    for (let j = 0; j < cartObj.length; j++) {
+      if (Number(cartObj[j]) === Number(itemsObj[i].id)) {
+        inCart = true;
+        break;
+      }
+    }    
+
+    // create new product for each item, append to product list
+    let item = new ProductItem(itemsObj[i].id, itemsObj[i].image, itemsObj[i].title, itemsObj[i].price, inCart); 
+    ul.appendChild(item);
+  } 
+}
